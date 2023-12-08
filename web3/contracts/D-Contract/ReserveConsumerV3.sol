59692869efd7e6fee3a637d7932e762da16192e1
DMind <dminddev01@gmail.com>
2023-12-08 11:34:02 +0330

    initializing reserver

---
+++ web3/contracts/D-Contract/ReserveConsumerV3.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract ReserveConsumerV3 {
+    
+    AggregatorV3Interface internal reserveFeed;
+
+    constructor() {
+        reserveFeed = AggregatorV3Interface(
+            0xa81FE04086865e63E12dD3776978E49DEEa2ea4e
+        );
+    }
+
+    function getLatestReserve() public view returns (int) {
+        (
+            uint80 roundID,
+            int reserve,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = reserveFeed.latestRoundData();
+
+        return reserve;
+    }
+}
---
+++ web3/contracts/D-Contract/reserver.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import {ReserveConsumerV3} from "./ReserveConsumerV3.sol";
+
+contract Custodian is ChainlinkClient, Ownable {
+
+  using Chainlink for Chainlink.Request;
+  using Strings for uint256;
+
+  address public oracle;
+  bytes32 public jobId;
+  uint256 public fee;
+
+  struct Request {
+    address sender;
+    uint256 tokenId;
+  }
+
+  mapping(bytes32 => Request) public requests;
+  mapping(uint256 => address) public owners;
+  uint256 public fulfills;
+
+  mapping(address => bool) public requestPending;
+
+  constructor() {}
+
+  function setJobConfig(
+    address _link,
+    address _oracle,
+    string memory _jobId,
+    uint256 _fee
+  ) public onlyOwner {
+    setChainlinkToken(_link);
+    oracle = _oracle;
+    jobId = stringToBytes32(_jobId);
+    fee = _fee;
+  }
+
+  function verify(uint256 tokenId) public {
+    address sender = address(this);
+
+    require(!requestPending[sender], "Sender already has a pending request");
+
+    Chainlink.Request memory request = buildChainlinkRequest(
+      jobId,
+      sender,
+      this.fulfill.selector
+    );
+    request.add("tokenId", tokenId.toString());
+
+    bytes32 requestId = sendChainlinkRequestTo(oracle, request, fee);
+    requests[requestId] = Request(sender, tokenId);
+    requestPending[sender] = true;
+  }
+
+  function fulfill(bytes32 requestId, bytes32 owner) public {
+    Request storage request = requests[requestId];
+
+    owners[request.tokenId] = address(uint160(uint256(owner)));
+    fulfills += 1;
+
+    delete requestPending[request.sender];
+  }
+
+  function ownerOf(uint256 tokenId) public view returns (address) {
+    return owners[tokenId];
+  }
+
+  function stringToBytes32(string memory source) private pure returns (bytes32 result) {
+    bytes memory tempEmptyStringTest = bytes(source);
+    if (tempEmptyStringTest.length == 0) {
+      return 0x0;
+    }
+
+    assembly { // solhint-disable-line no-inline-assembly
+      result := mload(add(source, 32))
+    }
+  }
+}
